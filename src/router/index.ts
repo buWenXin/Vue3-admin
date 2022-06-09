00e2b5a5f34593392ee0b306fee36e0f729aad5c
不问心 <71228657@qq.com>
2022-06-09 16:04:49 +0800

    1.3 添加element-icon

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,5 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import HomeView from '../views/HomeView.vue';
-
 import Home from "../views/base/home.vue"
-
-
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
--- src/views/base/login.vue
+++ src/views/base/login.vue
@@ -1,11 +1,32 @@
 <template>
-   <div>
-      login
+   <div style="width: 280px;height: 500px;margin:100px auto">
+      <el-form label-width="auto">
+         <el-form-item label="账号">
+            <el-input v-model="from.userName"/>
+         </el-form-item>
+         <el-form-item label="密码">
+            <el-input v-model="from.password" show-password/>
+         </el-form-item>
+         <el-form-item>
+            <div style="width: 100%;display: flex;justify-content: center">
+               <el-button style="width: 80%" round type="primary" @click="onSubmit">登录</el-button>
+            </div>
+         </el-form-item>
+      </el-form>
    </div>
 </template>
 
 <script lang="ts" setup>
+import {reactive} from "vue";
 
+const from = reactive({
+   userName: "",
+   password: ""
+});
+
+const onSubmit = () => {
+   console.log("登录");
+}
 </script>
 
 <style scoped>
