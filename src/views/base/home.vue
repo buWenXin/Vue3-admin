3b85e104dd60da2f4cb431fca5de91ab445020f2
不问心 <71228657@qq.com>
2022-06-09 16:22:47 +0800

    1.4 添加多环境

--- src/views/base/home.vue
+++ src/views/base/home.vue
@@ -1,11 +1,14 @@
 <template>
    <div>
       home
+      {{ref1}}
    </div>
 </template>
 
 <script lang="ts" setup>
+   import {ref} from "vue";
 
+   const ref1 = ref<string>(import.meta.env.VITE_TITLE);
 </script>
 
 <style scoped>
