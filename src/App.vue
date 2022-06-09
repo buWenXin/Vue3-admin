9f4abfb3cdf1b74d5033cb9e7fe86bbfd31d1aba
不问心 <71228657@qq.com>
2022-06-09 15:00:40 +0800

    1.3 添加element-icon

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,14 @@
 <template>
-   <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125"/>
-      <div class="wrapper">
-         <HelloWorld msg="You did it!"/>
-         <div style="width: 600px;">
-            <el-button type="primary" @click="three">改变主题色</el-button>
-            <el-button type="success" @click="open">弹出层</el-button>
-            <el-button type="info">Info</el-button>
-            <el-button type="warning">Warning</el-button>
-            <el-button type="danger">Danger</el-button>
-         </div>
-<!--         <nav>-->
-<!--            <RouterLink to="/">Home</RouterLink>-->
-<!--            <RouterLink to="/about">About</RouterLink>-->
-<!--         </nav>-->
-      </div>
-   </header>
-
-<!--   <RouterView/>-->
+   <div class="home_app">
+      <router-view v-slot="{ Component }">
+         <transition>
+            <component :is="Component"/>
+         </transition>
+      </router-view>
+   </div>
 </template>
 
 <script setup lang="ts">
-import {RouterLink, RouterView} from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
-import {useCssVar} from '@vueuse/core'
-import {ref} from "vue";
-
-function testClick() {
-   const key = ref('--el-color-success')
-   const colorVal = useCssVar(key)
-   colorVal.value = "red"
-}
-
-import {useDark} from '@vueuse/core';
-
-const isDark = useDark()
-
-function three() {
-   isDark.value = !isDark.value;
-}
-
-/**
- * ------------------------------------------------------------<-->----------------------------------------------------------------------------------
- */
-import {ElMessage, ElMessageBox} from 'element-plus'
-
-const open = () => {
-   ElMessageBox.confirm(
-         'proxy will permanently delete the file. Continue?',
-         'Warning',
-         {
-            confirmButtonText: 'OK',
-            cancelButtonText: 'Cancel',
-            type: 'warning',
-         }
-   ).then(() => {
-      ElMessage({
-         type: 'success',
-         message: 'Delete completed',
-      })
-   }).catch(() => {
-      ElMessage({
-         type: 'info',
-         message: 'Delete canceled',
-      })
-   })
-}
-
 
 </script>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import HomeView from '../views/HomeView.vue';
+
+import Home from "../views/base/home.vue"
+
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: '首页',
+            component: Home
+        },
+        {
+            path: '/login',
+            name: '登录',
+            component: () => import("@/views/base/login.vue")
+        }
+    ]
 })
 
 export default router
