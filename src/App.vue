55a3d076f65736636ab01e9281381325044d04a2
不问心 <71228657@qq.com>
2022-06-09 14:47:38 +0800

    1.3 添加element-icon

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,19 @@
          <HelloWorld msg="You did it!"/>
          <div style="width: 600px;">
             <el-button type="primary" @click="three">改变主题色</el-button>
-            <el-button type="success">Success</el-button>
+            <el-button type="success" @click="open">弹出层</el-button>
             <el-button type="info">Info</el-button>
             <el-button type="warning">Warning</el-button>
             <el-button type="danger">Danger</el-button>
          </div>
-
-         <nav>
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
-         </nav>
+<!--         <nav>-->
+<!--            <RouterLink to="/">Home</RouterLink>-->
+<!--            <RouterLink to="/about">About</RouterLink>-->
+<!--         </nav>-->
       </div>
    </header>
 
-   <RouterView/>
+<!--   <RouterView/>-->
 </template>
 
 <script setup lang="ts">
@@ -39,6 +38,33 @@ const isDark = useDark()
 
 function three() {
    isDark.value = !isDark.value;
+}
+
+/**
+ * ------------------------------------------------------------<-->----------------------------------------------------------------------------------
+ */
+import {ElMessage, ElMessageBox} from 'element-plus'
+
+const open = () => {
+   ElMessageBox.confirm(
+         'proxy will permanently delete the file. Continue?',
+         'Warning',
+         {
+            confirmButtonText: 'OK',
+            cancelButtonText: 'Cancel',
+            type: 'warning',
+         }
+   ).then(() => {
+      ElMessage({
+         type: 'success',
+         message: 'Delete completed',
+      })
+   }).catch(() => {
+      ElMessage({
+         type: 'info',
+         message: 'Delete canceled',
+      })
+   })
 }
 
 
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,12 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+/**
+ * ------------------------------------------------------------<-element-plus 样式表->----------------------------------------------------------------------------------
+ */
 
-import 'element-plus/theme-chalk/dark/css-vars.css'
+import 'element-plus/theme-chalk/dark/css-vars.css';
+import 'element-plus/dist/index.css'
 
 app.use(createPinia())
 app.use(router)
