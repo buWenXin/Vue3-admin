df1882709e5fd66c3d1005312790d39f545cabde
不问心 <71228657@qq.com>
2022-06-13 10:16:54 +0800

    1.5.1 添加登录

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,29 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import type {RouteLocationNormalized} from "vue-router";
-import Home from "@/views/Home.vue"
+
 import {MyCache} from "@/utils/MyCache";
 import {useUserStore} from "@/stores/user";
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
+        //配置路由映射
         {
-            path: '/',
-            name: '首页',
-            component: Home
+            path: "/",
+            redirect: "/home/index"
+        },
+        {
+            path: "/home",
+            name: "home",
+            component: () => import("@/views/Home.vue"),
+            redirect: "/home/index",
+            children: [
+                {
+                    path: "index",
+                    meta: {title: "首页"},
+                    component: ()=>import("@/views/base/index/Index.vue"),
+                },
+            ]
         },
         {
             path: '/login',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,17 @@
       home
       {{ref1}}
    </div>
+   <div>
+      <router-view v-slot="{ Component }">
+         <transition>
+            <component :is="Component"/>
+         </transition>
+      </router-view>
+   </div>
 </template>
 
 <script lang="ts" setup>
    import {ref} from "vue";
-
    const ref1 = ref<string>(import.meta.env.VITE_TITLE);
    console.log(import.meta.env.VITE_TEST);
 </script>
