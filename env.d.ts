d08d2f72349e666dbcd5b9f1b313bb549a4cfd00
不问心 <71228657@qq.com>
2022-06-14 10:50:30 +0800

    1.5.3 解决组件类型报错

--- env.d.ts
+++ env.d.ts
@@ -11,3 +11,10 @@ interface ImportMetaEnv {
 interface ImportMeta {
     readonly env: ImportMetaEnv
 }
+//解决组件类型报错
+declare module "*.vue" {
+   import { DefineComponent } from "vue";
+   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+   const component: DefineComponent<{}, {}, any>;
+   export default component;
+}
