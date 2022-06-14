3e8ed90c52750381e14c281d085a5f9259d7a47e
不问心 <71228657@qq.com>
2022-06-14 11:59:09 +0800

    1.5.3 添加代理服务器

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { fileURLToPath, URL } from 'url'
+import {fileURLToPath, URL} from 'url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 /**
@@ -8,23 +8,33 @@ import vue from '@vitejs/plugin-vue'
  */
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
-  plugins: [
+   resolve: {
+      alias: {
+         '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+   },
+   plugins: [
       vue(),
       //按需导入插件
       AutoImport({
-          resolvers: [ElementPlusResolver()],
+         resolvers: [ElementPlusResolver()],
       }),
       //按需导入插件
       Components({
-          resolvers: [ElementPlusResolver()],
+         resolvers: [ElementPlusResolver()],
       }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+   ],
+   server: {
+      proxy: {
+         // 选项写法
+         '/dev-api': {
+            target: 'http://192.168.8.115:12306',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/dev-api/, '')
+         },
+      }
+   }
 })
